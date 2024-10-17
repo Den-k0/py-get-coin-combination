a1c2fbb4a9b89dafb5b2bb7afd5b4dd5451e8338
Den_k0 <denys.kotovskiy@gmail.com>
2024-10-17 17:24:26 +0300

    Solution fix

--- app/test_main.py
+++ app/test_main.py
@@ -8,13 +8,21 @@ from app.main import get_coin_combination
         (1, [1, 0, 0, 0]),
         (5, [0, 1, 0, 0]),
         (10, [0, 0, 1, 0]),
-        (25, [0, 0, 0, 1])
+        (25, [0, 0, 0, 1]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (42, [2, 1, 1, 1]),
+        (543, [3, 1, 1, 21])
     ],
     ids=[
         "return only pennies",
         "return only nickels",
         "return only dimes",
         "return only quarters",
+        "return pennies and nickels",
+        "return pennies, nickels and dimes",
+        "return pennies, nickels, dimes and quarters",
+        "return big number of pennies, nickels, dimes and quarters"
     ]
 )
 def test_coin_combination(
